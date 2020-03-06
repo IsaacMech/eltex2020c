684f763a2f92469899828c49cfe90298e3be43a3
IsaacMech <weburda@ya.ru>
2020-03-06 19:15:23 +0700

    Наконец-то работает получение символа в кодировке utf8

--- src/lab1/test.c
+++ src/lab1/test.c
@@ -2,9 +2,11 @@
 
 int main() {
     FILE *input = fopen("codex.txt", "r");
+    for(int j = 0; j < 8; j++) {
     struct char_utf8 buf = fgetc_utf8(input);
     for(int i = 0; i < buf.size; i++) {
-        putc(*(buf.symbol + i));
+        putc(*(buf.symbol + i), stdout);
+    }
     }
 
     return 0;
