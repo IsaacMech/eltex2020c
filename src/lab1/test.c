508f933bac323cf6a229ba06df6171cda1c591d5
IsaacMech <41058460+IsaacMech@users.noreply.github.com>
2020-03-06 11:08:15 +0700

    Update test.c

--- src/lab1/test.c
+++ src/lab1/test.c
@@ -4,7 +4,7 @@ int main() {
     FILE *input = fopen("codex.txt", "r");
     struct char_utf8 buf = fgetc_utf8(input);
     for(int i = 0; i < buf.size; i++) {
-        putc(*(buf.symbol + i);
+        putc(*(buf.symbol + i));
     }
 
     return 0;
